8eaf01f7e56fa9214318d1bbc857958885024514
sou <sou.phphp@gmail.com>
2023-03-16 18:29:45 +0900

    add mint-nft

--- solidity/my-first-nft/hardhat.config.ts
+++ solidity/my-first-nft/hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
+// import { API_URL } from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  // defaultNetwork: "sepolia",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  }
 };
 
 export default config;
--- solidity/my-first-nft/scripts/deploy.ts
+++ solidity/my-first-nft/scripts/deploy.ts
@@ -1,23 +1,16 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+async function main(): Promise<void> {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    const myNFT = await MyNFT.deploy();
+    await myNFT.deployed();
+    console.log("Contract deployed to:", myNFT.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+    .then(() => process.exit(0))
+    .catch((error: Error) => {
+        console.error(error);
+        process.exit(1);
+    });
