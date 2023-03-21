123cc41761f85ae7bcee69ae90748ab10b9bdc39
sou <sou.phphp@gmail.com>
2023-03-21 11:36:11 +0900

    add ERC1155 contract

--- solidity/demo-erc1155/scripts/deploy.ts
+++ solidity/demo-erc1155/scripts/deploy.ts
@@ -1,24 +1,21 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const factory = await ethers.getContractFactory("AwesomeGame");
+  const [owner] = await ethers.getSigners();
+  const contract = await factory.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await contract.deployed();
+  console.log("Contract deployed to:", contract.address);
+  console.log("Contract deployed by:", owner.address);
+  console.log("Tokens have been mented");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main()
+.then(() => process.exit(0))
+.catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
